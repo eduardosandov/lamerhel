481bf045abca7759154c253b8cd03a565725e325
Eduardo Sandoval <eduardosandoval.atc@gmail.com>
2021-07-03 16:18:26 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -5,7 +5,6 @@ if (isset($_POST['enviar'])) {
         $name = $_POST['name'];
         $asunto = $_POST['asunto'];
         $msg = $_POST['msg'];
-        $name = $_POST['name'];
         $email = $_POST['email'];
         $header = "From: sistemas@labatc.com" . "\r\n";
         $header.= "Reply-To: sistemas@labatc.com" . "\r\n";
